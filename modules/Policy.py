84add411738909b90c7c393f01f345363f433d9c
mjbooo <slalektm@gmail.com>
2024-05-11 00:11:28 +0000

    to cuda for gpu

--- modules/Policy.py
+++ modules/Policy.py
@@ -53,6 +53,27 @@ class Policy(object):
         mask: torch.BoolTensor,
     ) -> torch.FloatTensor:
         # 1. exact reward
+        q_drf_labels = torch.gather(q_drf, -1, labels_drf.unsqueeze(-1)).squeeze(-1)
+        p_tgt_labels = torch.gather(p_tgt, -1, labels_drf.unsqueeze(-1)).squeeze(-1)
+
+        probability_ratio = p_tgt_labels / q_drf_labels
+        mask = mask.to(probability_ratio.device)
+
+        # Don't count the padding tokens for the exact reward
+        probability_ratio[mask] = 0
+
+        acceptance_ratio = torch.min(probability_ratio, torch.tensor(1))
+
+        return torch.cumprod(acceptance_ratio, dim=1).sum(dim=1)
+    
+    def get_improved_reward(
+        self,
+        q_drf: torch.FloatTensor,
+        p_tgt: torch.FloatTensor,
+        labels_drf: torch.LongTensor,
+        mask: torch.BoolTensor,
+    ) -> torch.FloatTensor:
+        # 1. exact reward
         q_drf_labels = torch.gather(q_drf, -1, labels_drf.unsqueeze(-1)).squeeze(-1).cpu()
         p_tgt_labels = torch.gather(p_tgt, -1, labels_drf.unsqueeze(-1)).squeeze(-1).cpu()
 
